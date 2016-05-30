207f9ab457bbea9cd1eb1358430c7fe1cef4156d
Raf Szalanski <rafal.szalanski@alliants.com>
2016-05-30 20:17:19 +0100

    Refer to one symbol when talking about encryption/decryption key

--- src/snap/messages_safe.clj
+++ src/snap/messages_safe.clj
@@ -5,11 +5,13 @@
 (def server-conn {:spec {:host "127.0.0.1" :port 6379}})
 (defmacro wcar* [& body] `(car/wcar server-conn ~@body))
 
+(def encryption-key "one-two-three-is-the-key")
+
 (defn store
   "Encrypts and stores message under UUID key and returns the key."
   [message]
   (let [uuid (str (java.util.UUID/randomUUID))
-        encrypted-message (cipher/encrypt message "one-two-three-is-the-key")]
+        encrypted-message (cipher/encrypt message encryption-key)]
     (wcar*
      (car/set uuid {:encrypted-message encrypted-message}))
     uuid))
@@ -19,5 +21,4 @@
   [uuid]
   (let [stored-message (wcar* (car/get uuid))]
     (when stored-message
-      (String. (cipher/decrypt (:encrypted-message stored-message) "one-two-three-is-the-key")))))
-
+      (String. (cipher/decrypt (:encrypted-message stored-message) encryption-key)))))
