b6133ab53021faae8ab0b114bdc232fd68748075
Raf Szalanski <rafal.szalanski@alliants.com>
2016-05-31 22:35:31 +0100

    Add default bootstrap layout and use it to render index page

--- src/snap/actions/index.clj
+++ src/snap/actions/index.clj
@@ -1,17 +1,19 @@
 (ns snap.actions.index
   (:require [hiccup.core :refer [html]]
             [hiccup.form :refer [form-to]]
-            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [snap.layout :as layout]))
 
 (defn view []
-  [:html
-   [:body
-    [:div
-     [:p "Please enter the message in text area below:"]
+  [:div {:class "container"}
+   [:div {:class "row"}
+    [:div {:class "col-md-6 col-md-offset-3"}
      (form-to [:post "/messages"]
-      (anti-forgery-field)
-      [:textarea {:name "message"}]
-      [:button {:type "submit"} "Save"])]]])
+              (anti-forgery-field)
+              [:div {:class "form-group"}
+               [:label {:for "message"} "Please enter message you would like to encrypt"]
+               [:textarea {:name "message" :class "form-control" :rows 5}]]
+              [:button {:class "btn btn-default" :type "submit"} "Submit"])]]])
 
 (defn perform [_]
-  (html (view)))
+  (html (layout/default-bootstrap (view))))
