4ec785b5df449ad26b718e37288bdffd1984746b
Raf Szalanski <rafal.szalanski@alliants.com>
2016-05-31 22:38:04 +0100

    Add bootstrap theme to store view

--- src/snap/actions/store.clj
+++ src/snap/actions/store.clj
@@ -1,15 +1,17 @@
 (ns snap.actions.store
   (:require [hiccup.core :refer [html]]
+            [snap.layout :as layout]
             [snap.messages-safe :as messages-safe]))
 
 (defn view [uuid]
-  [:html
-   [:body
-    [:div
-     [:p "Your message has been encrypted and persisted."]
-     [:p (str "It's available under the following url: HOST/messages/" uuid)]]]])
+  [:div {:class "container"}
+   [:div {:class "row"}
+    [:div {:class "col-md-6 col-md-offset-3"}
+     [:h3 "Your message has been encrypted and stored!"]
+     [:p (str "It's available under this url: " "http://localhost:3000" "/messages/" uuid)]
+     [:p "The message will be destroyed when it's first read so DO NOT try accessing this url before the recipient."]]]])
 
 (defn perform [req]
   (let [message (get (:params req) :message)]
     (when message
-      (html (view (messages-safe/store message))))))
+      (html (layout/default-bootstrap (view (messages-safe/store message)))))))
